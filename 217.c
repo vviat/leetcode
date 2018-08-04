c9124cbc413aa094fb32dbdadaef2aef0f39c21f
ygy <40139861+vviat@users.noreply.github.com>
2018-08-04 23:00:41 +0800

    存在重复元素

---
+++ 217.c
@@ -0,0 +1,10 @@
+bool containsDuplicate(int* nums, int numsSize) {
+    int i=0, j=0;
+    for(i=0; i < numsSize; i++) {
+        for(j = i + 1; j < numsSize; j++) {
+            if(nums[i] == nums[j])
+                return true;
+        }
+    }
+    return false;
+}
